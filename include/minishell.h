d86faec95d776da4ca3d7a0bcabaa36a509ecc80
Justin Hille <jhille@f1r2s9.codam.nl>
2022-08-18 12:03:48 +0200

    Created a t_data struct

---
+++ include/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jhille <jhille@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 11:53:21 by jhille        #+#    #+#                 */
+/*   Updated: 2022/08/18 12:03:14 by jhille        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "utils.h"
+# include "parser.h"
+
+typedef struct s_data
+{
+	t_token	*lst;
+	t_ast	*tree;
+	char	*envv;
+}				t_data;
+
+#endif
